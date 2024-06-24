356413878c6219e1b5ce80e228ac7e55a68d4597
DeHopen <a2302004b23@yandex.ru>
2024-06-24 14:03:26 +0300

    HotFix 0.4: Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
-        });
+    swcMinify: true,
+    webpack: (config, {isServer}) => {
+        if (isServer) {
+            config.module.rules.push({
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        publicPath: '/_next/static/images',
+                        outputPath: 'static/images',
+                        name: '[name].[hash].[ext]',
+                        esModule: false,
+                    },
+                },
+            });
+        }
 
         return config;
     }
