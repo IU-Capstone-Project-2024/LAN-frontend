5a15226add87a3952d4abd273ceaee5377631f2b
Gerasimov Andrey <46645241+DeHopen@users.noreply.github.com>
2024-07-01 23:23:13 +0300

    Edit profile styles and fix bugs

--- src/components/Profile/CarouselElement/CarouselElement.tsx
+++ src/components/Profile/CarouselElement/CarouselElement.tsx
@@ -1,16 +1,17 @@
 import React, {FC} from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import styles from "@/Styles/Profile/profile.module.scss";
+import styles from "@/Styles/Profile/Carousel.module.scss";
 import {useSelector} from "react-redux";
 import {RootState} from "@/Store/store";
 import Image from "next/image";
+import '@/Styles/Profile/castomCarousel/castomCarousel.css'
 
 const CarouselElement: FC = () => {
   const profile = useSelector((state: RootState) => state.profile);
 
   return (
-      <div>
+      <>
         {profile.photos.length > 0 ? (
             <Carousel showThumbs={false} showArrows={false} showStatus={false}>
               {profile.photos.map((photo, index) => (
@@ -24,7 +25,7 @@ const CarouselElement: FC = () => {
             <Image src='/default_avatar.png' alt="Placeholder" width={600} height={600}/>
 
         )}
-      </div>
+      </>
   );
 };
 
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,20 +1,15 @@
 "use client"
 
-import React, {FC, useEffect, useState} from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/Store/store';
+import React, { FC, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import styles from '../../Styles/Profile/profile.module.scss';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import CarouselElement from "@/components/Profile/CarouselElement/CarouselElement";
-import ReadOnlySlider from "@/components/Profile/ReadOnlySlider/ReadOnlySlider";
-import Image from "next/image";
+import ProfileInfo from "@/components/Profile/ProfileInfo/ProfileInfo";
+import ProfileHeader from "@/components/Profile/ProfileHeader/ProfileHeader";
 
 const Profile: FC = () => {
-  const profile = useSelector((state: RootState) => state.profile);
   const router = useRouter();
-  const [screenWidth, setScreenWidth] = useState<number>(0);
-
+  const [screenWidth] = useState<number>(0);
   const handleEditProfile = () => {
     router.push('/profile_settings');
   };
@@ -27,92 +22,10 @@ const Profile: FC = () => {
     }
   };
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setScreenWidth(window.innerWidth);
-
-      const handleResize = () => setScreenWidth(window.innerWidth);
-      window.addEventListener('resize', handleResize);
-
-      return () => window.removeEventListener('resize', handleResize);
-    }
-  }, []);
-
-  const defaultAge = 'Возраст не указан';
-  const defaultReligion = 'Религия не указана';
-
-  const renderAgeReligion = () => {
-    const age = profile.age || defaultAge;
-    const religion = profile.religion || defaultReligion;
-
-    if (!profile.age && !profile.religion) {
-      return <p className={styles['age']}>0 лет</p>;
-    }
-
-    if (profile.age && !profile.religion) {
-      return <p className={styles['age']}>{age} лет</p>;
-    }
-
-    return <p className={styles['age']}>{age} лет | {religion}</p>;
-  };
-
   return (
       <div className={styles.profile}>
-        <div className={styles['header-container']}>
-          <div className={styles['header']}>
-            <span>
-              Профиль
-            </span>
-            <button className={styles['icon']} onClick={handleEditProfile}>
-              <Image src='/Settings_icon.svg' alt='Настройки' width={30} height={30} />
-            </button>
-          </div>
-          <CarouselElement/>
-        </div>
-        { profile.name ? (
-            <h1>{profile.name}</h1>
-        ) : (
-            <h1>Имя</h1>
-        )}
-        {renderAgeReligion()}
-        { profile.about && (
-            <div className={styles["about"]}>
-              <h2>О себе</h2>
-              <p>{profile.about}</p>
-            </div>
-        )}
-        { profile.interests.length > 0 && (
-            <div className={styles["interests"]}>
-              <h2>Интересы</h2>
-              <div className={styles["interests-list"]}>
-                {profile.interests.map((interest, index) => (
-                    <span key={index}>{interest}</span>
-                ))}
-              </div>
-            </div>
-        )}
-        <div className={styles["co-life"]}>
-          <h2>Co-Life</h2>
-          <ReadOnlySlider labelStart="Сова" labelEnd="Жаворонок" value={profile.coLife.nightOwl} max={2}/>
-          <ReadOnlySlider labelStart="Чистота важна" labelEnd="Чистота не так важна" value={profile.coLife.cleanliness} max={2} />
-          <ReadOnlySlider labelStart="Люблю тишину и покой" labelEnd="Люблю шум и компанию" value={profile.coLife.noiseLevel} max={2} />
-          <ReadOnlySlider labelStart="Пью алкоголь" labelEnd="Не пью" value={profile.coLife.alcohol} max={2} />
-          <ReadOnlySlider labelStart="Курю" labelEnd="Не курю" value={profile.coLife.smoking} max={2} />
-        </div>
-        { profile.socialLinks.length > 0 && (
-            <div className={styles["social-links"]}>
-              <h2>Соц. сети</h2>
-              <ul>
-                {profile.socialLinks.map((link, index) => (
-                    <li key={index}>
-                      <a href={link} target="_blank" rel="noopener noreferrer">
-                        {truncateLink(link)}
-                      </a>
-                    </li>
-                ))}
-              </ul>
-            </div>
-        )}
+        <ProfileHeader onEditProfile={handleEditProfile}/>
+        <ProfileInfo screenWidth={screenWidth} truncateLink={truncateLink}/>
       </div>
   );
 };
