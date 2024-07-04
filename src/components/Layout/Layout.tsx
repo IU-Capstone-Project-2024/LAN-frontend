db7b5f1ee2bf7b4eceb785f568ff040633051c8a
DeHopen <a2302004b23@yandex.ru>
2024-07-05 01:37:13 +0300

    Add notification

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, {useState} from "react";
 import {usePathname, useRouter} from "next/navigation";
 import styles from "@/Styles/Layout.module.scss";
 
@@ -8,26 +8,33 @@ const icons = [
   { name: "поиск", icon: "/layout/PassiveIcons/dating.svg", activeIcon: "/layout/ActiveIcons/dating.svg", route: "/dating" },
   { name: "чат", icon: "/layout/PassiveIcons/favorites.svg", activeIcon: "/layout/ActiveIcons/favorites.svg", route: "/favorite" },
   { name: "профиль", icon: "/layout/PassiveIcons/profile.svg", activeIcon: "/layout/ActiveIcons/profile.svg", route: "/profile" },
-  { name: "комната", icon: "/layout/PassiveIcons/room.svg", activeIcon: "/layout/ActiveIcons/room.svg", route: "/room" },
+  { name: "комната", icon: "/layout/PassiveIcons/room.svg", activeIcon: "/layout/ActiveIcons/room.svg", route: "#" },
   { name: "настройки", icon: "/layout/PassiveIcons/settings.svg", activeIcon: "/layout/ActiveIcons/settings.svg", route: "/settings" }
 ];
 
 const Layout = () => {
   const router = useRouter();
   const pathname = usePathname();
+  const [showNotification, setShowNotification] = useState(false);
 
   const handleNavigation = (route: string) => {
-    router.push(route);
+    if (route === "#") {
+      setShowNotification(true);
+      setTimeout(() => setShowNotification(false), 3000);
+    } else {
+      router.push(route);
+    }
   };
 
   return (
       <div className={styles.container}>
+        <main className={styles.content}></main>
         <footer className={styles.footer}>
-          <ul className={styles["footerIconList"]}>
+          <ul className={styles.footerIconList}>
             {icons.map((item, index) => (
                 <li
                     key={index}
-                    className={styles["footerIconItem"]}
+                    className={styles.footerIconItem}
                     onClick={() => handleNavigation(item.route)}
                 >
                   <img
@@ -37,9 +44,12 @@ const Layout = () => {
                 </li>
             ))}
           </ul>
-          <div className={styles['gradient']}></div>
         </footer>
-
+        {showNotification && (
+            <div className={styles.notification}>
+              <p>Скоро</p>
+            </div>
+        )}
       </div>
   );
 };
