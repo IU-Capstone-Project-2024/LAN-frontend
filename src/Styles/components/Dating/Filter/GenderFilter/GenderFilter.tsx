085aa98ba6cd9571e2a06ef8be9f627d4d0b09a9
Кирилл Архипов <120665289+MarketerKA@users.noreply.github.com>
2024-07-17 01:34:56 +0300

    Страница стилей

---
+++ src/Styles/components/Dating/Filter/GenderFilter/GenderFilter.tsx
@@ -0,0 +1,52 @@
+import React, { FC, ChangeEvent } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/Store/store';
+import { setGender } from '@/Store/slices/filterSlice';
+import styles from '@/Styles/Dating/Filter/GenderFilter.module.scss';
+
+const GenderFilter: FC = () => {
+  const dispatch = useDispatch();
+  const gender = useSelector((state: RootState) => state.filters.gender);
+
+  const handleGenderChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch(setGender(e.target.value));
+  };
+
+  return (
+      <div className={styles['gender-group']}>
+        <label>Пол</label>
+        <label>
+          <input
+              type="radio"
+              name="gender"
+              value="Мужской"
+              checked={gender === 'Мужской'}
+              onChange={handleGenderChange}
+          />
+          Мужской
+        </label>
+        <label>
+          <input
+              type="radio"
+              name="gender"
+              value="Женский"
+              checked={gender === 'Женский'}
+              onChange={handleGenderChange}
+          />
+          Женский
+        </label>
+        <label>
+          <input
+              type="radio"
+              name="gender"
+              value="Любой"
+              checked={gender === 'Любой'}
+              onChange={handleGenderChange}
+          />
+          Любой
+        </label>
+      </div>
+  );
+};
+
+export default GenderFilter;
